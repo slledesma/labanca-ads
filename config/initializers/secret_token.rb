d2b6eeed81c175e058373519e22f71f625e448a8
Seba Ledesma <slledesma@gmail.com>
2011-04-29 11:03:45 -0300

    New ads app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LabancaAds::Application.config.secret_token = '1c6ebddfb6499425abd6f27c7428112557dbf13f48e8fbe3ac3a4d5a31ddee67b1a1cc1d52127add0ee38f6bd9d94d741c69acb32ef14032447a09b30986fc7d'
