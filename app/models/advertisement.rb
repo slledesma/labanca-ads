f013e1599d54f782659440a486e8e20e5cbf8de7
Seba Ledesma <slledesma@gmail.com>
2011-05-09 10:14:11 -0300

    scope parejo

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,3 +1,8 @@
 class Advertisement < ActiveRecord::Base
   belongs_to:user
+  validates :price, :numericality=>{:greater_than=>0}
+  scope :expensive, where("price >= 500")
+  scope :regular, where(:price => (100..500))
+  scope :cheap, where("price <= 100")  
+
 end
--- spec/models/advertisement_spec.rb
+++ spec/models/advertisement_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe Advertisement do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "with a price <=0" do
+    before do
+      subject.price=0
+    end 	
+
+    it "should not be valid" do
+      subject.should have(1).error_on(:price)
+    end
+  end
+
+context "with a price >0" do
+    before do
+      subject.price=1
+    end 	
+
+    it "should be valid" do
+      subject.should be_valid
+    end
+  end
+
 end
