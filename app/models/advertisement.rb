c1f395aabc26628872ca11507254fc3eaeedc69b
Seba Ledesma <slledesma@gmail.com>
2011-05-09 11:04:10 -0300

    vemos

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -3,6 +3,20 @@ class Advertisement < ActiveRecord::Base
   validates :price, :numericality=>{:greater_than=>0}
   scope :expensive, where("price >= 500")
   scope :regular, where(:price => (100..500))
-  scope :cheap, where("price <= 100")  
+  scope :cheap, where("price <= 100")
+  scope :by_price, order(:price)    
+
+  before_save :capitalize_title
+
+## para editar el setter
+#  def title=(value)
+#    write_attribute(:title, value.capitalize)
+#  end
+
+  private 
+   def capitalize_title
+     logger.debug "Estoy en capitalize_title"
+     self.title = title.capitalize
+   end
 
 end
